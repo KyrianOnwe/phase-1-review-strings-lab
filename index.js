d7d68c3de1ed7bee67831c524b1a64cfd16aa2ae
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-24 22:59:42 -0600

    commit tree 181ea76d5bade1b3e8fb2c8fb6d36cdc53c75992

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code in this file!
-let currentUser = "BARIO BOYS";
+/*let currentUser = "BARIO BOYS";
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
-let shortGreeting = "Welcome, " + currentUser[0] + '!'
+let shortGreeting = "Welcome, " + currentUser[0] + '!'*/
+let currentUser = "Chuck";
+let welcomeMessage = `Welcome to Flatbook, ya noobs and ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser[0]}!`
